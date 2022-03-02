b7ef93d3b8fd7aa484a6340ec04373acb4a6bb58
mv555 <vekariyamadhav@gmail.com>
2022-03-02 17:01:15 +0530

    Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:play_it/Appbar/my_title.dart';
 import 'package:play_it/homepage.dart';
+
+import 'Madhav/Appbar/my_title.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: Colors.black,
+      ),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+       home: MyTitle(),
+      //home: Home(),
     ),
   );
 }
