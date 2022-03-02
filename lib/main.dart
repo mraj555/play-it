845e4678bbff41ca69a48022bbd4f6a81a30f343
mraj555 <jenilambaliya999@gmail.com>
2022-03-02 16:18:23 +0530

    File Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:play_it/Appbar/my_title.dart';
+import 'package:play_it/homepage.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home: MyTitle(),
+      home: Home(),
     ),
   );
 }
