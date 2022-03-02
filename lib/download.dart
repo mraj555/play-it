1c29cd034937fa60cbb468e89fb6df122c9036a0
mraj555 <jenilambaliya999@gmail.com>
2022-03-02 16:00:33 +0530

    File Added

---
+++ lib/download.dart
@@ -0,0 +1,412 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:play_it/homepage.dart';
+
+class Down extends StatefulWidget {
+  const Down({Key? key}) : super(key: key);
+
+  @override
+  _DownState createState() => _DownState();
+}
+
+class _DownState extends State<Down> {
+  var groupValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Home()));
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Download',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                SizedBox(width: 80),
+                IconButton(
+                  onPressed: () => _Openfile(),
+                  icon: Icon(
+                    Icons.folder_open,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => _openadd(),
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.settings_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 250,
+            ),
+            Text(
+              'No File',
+              style: TextStyle(color: Colors.grey),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                  (route) => false,
+                );
+              },
+              child: Text(
+                'Go To Download',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(primary: Colors.green),
+            ),
+            SizedBox(
+              height: 230,
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.phone_android_sharp,
+                color: Colors.white,
+              ),
+              trailing: Icon(Icons.navigate_next_outlined),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _Openfile() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SafeArea(
+          child: Scaffold(
+            body: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => Down()),
+                          (route) => false,
+                        );
+                      },
+                      icon: Icon(
+                        Icons.arrow_back_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Privacy Folder',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(width: 80),
+                    IconButton(
+                      onPressed: () => showDialog(
+                        context: context,
+                        builder: (context) => StatefulBuilder(
+                          builder: (context, setState) => Dialog(
+                            child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 10, left: 10),
+                                    child: Text(
+                                      'Sort By',
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: 18),
+                                    ),
+                                  ),
+                                  RadioListTile(
+                                      controlAffinity:
+                                          ListTileControlAffinity.trailing,
+                                      title: Text('Date'),
+                                      activeColor: Colors.green,
+                                      value: 1,
+                                      groupValue: groupValue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          groupValue = value as int;
+                                        });
+                                      }),
+                                  RadioListTile(
+                                      controlAffinity:
+                                          ListTileControlAffinity.trailing,
+                                      title: Text('Name'),
+                                      activeColor: Colors.green,
+                                      value: 2,
+                                      groupValue: groupValue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          groupValue = value as int;
+                                        });
+                                      }),
+                                  RadioListTile(
+                                      controlAffinity:
+                                          ListTileControlAffinity.trailing,
+                                      title: Text('Size'),
+                                      activeColor: Colors.green,
+                                      value: 3,
+                                      groupValue: groupValue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          groupValue = value as int;
+                                        });
+                                      }),
+                                  RadioListTile(
+                                      controlAffinity:
+                                          ListTileControlAffinity.trailing,
+                                      title: Text('Length'),
+                                      activeColor: Colors.green,
+                                      value: 4,
+                                      groupValue: groupValue,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          groupValue = value as int;
+                                        });
+                                      }),
+                                  Divider(
+                                    color: Colors.green,
+                                    indent: 20,
+                                    endIndent: 20,
+                                  ),
+                                ]),
+                          ),
+                        ),
+                      ),
+                      icon: Icon(
+                        Icons.grid_view,
+                        color: Colors.white,
+                      ),
+                    ),
+                    PopupMenuButton(
+                      itemBuilder: (context) => [
+                        PopupMenuItem(
+                          onTap: () => _setpassword(),
+                          child: Text('Set Password'),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ),
+              backgroundColor: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _openadd() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SafeArea(
+          child: Scaffold(
+            body: ListView(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(builder: (context) => Down()),
+                              (route) => false,
+                            );
+                          },
+                          icon: Icon(
+                            Icons.arrow_back_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'Add Download Link',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.insert_drive_file_rounded,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => _search(),
+                          icon: Icon(
+                            Icons.search_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      height: 200,
+                      width: 350,
+                      color: Colors.black,
+                      margin: EdgeInsets.all(10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 10),
+                            hintText:
+                                'Enter The Magnet ,HTTP Or The URL of Torrent',
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 15),
+                            border: InputBorder.none),
+                        cursorColor: Colors.green,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 340,
+                      color: Colors.green,
+                      child: TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.download,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Download',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _search() {
+    return showDialog(
+      context: context,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setState) => Dialog(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    'Search',
+                    style: TextStyle(color: Colors.green, fontSize: 18),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 10),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Colors.green)),
+                      border: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 2, color: Colors.green))),
+                  cursorColor: Colors.green,
+                ),
+                ButtonBar(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Save',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _setpassword() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          body: ListView(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text('Change Password ')
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:play_it/download.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _image1 = [
+    'https://png.pngtree.com/element_our/md/20180626/md_5b321c98efaa6.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGSHnjhohZvjxf19zHrR8UKszTq5mIfH9YJQ&usqp=CAU',
+    'https://ichef.bbci.co.uk/news/976/cpsprodpb/E802/production/_89649395_instagram_logo_976.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/YouTube_social_white_square_%282017%29.svg/2048px-YouTube_social_white_square_%282017%29.svg.png',
+    'https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.1920.jpg',
+    'https://image.winudf.com/v2/image/Y29tLmluZGlyLm1wM3R1YmlkeV9zY3JlZW5fM19lZzhrNnZxdw/screen-3.jpg?fakeurl=1&type=.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8opJd7fcSRwYrFu028Ap7ZaLlzyiG5eANsN0V5yJlk_qupJk1DxDymuWnnOsyb5czKB8&usqp=CAU',
+    'https://sg-res.9appsdownloading.com/sg/res/jpg/5e/ad/0c6dd1530ee8268583681c94b5c7-sfq.jpg?x-oss-process=style/mq200',
+  ];
+  List name1 = [
+    'Whatsapp',
+    'Facebook',
+    'Instagram',
+    'Youtube',
+    'Twitter',
+    'Tubidy',
+    'TED',
+    'LinkFly VPN'
+  ];
+  var namecontroller = TextEditingController();
+  var name = '';
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: _opensearch,
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 10),
+                      height: 40,
+                      width: 280,
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(width: 3, color: Colors.green)),
+                      child: Icon(
+                        Icons.search_rounded,
+                        color: Colors.grey,
+                        size: 18,
+                      )),
+                ),
+                IconButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => Down()),
+                          (route) => false);
+                    },
+                    icon: Icon(
+                      Icons.download,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              margin: EdgeInsets.only(top: 30),
+              child: Wrap(
+                runSpacing: 20,
+                children: List.generate(
+                  _image1.length,
+                  (index) => Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 20, right: 20),
+                        child: CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(_image1[index]),
+                        ),
+                      ),
+                      Text(
+                        name1[index],
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, bottom: 10),
+              child: Text(
+                'Book Mark',
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  FloatingActionButton(
+                    backgroundColor: Colors.grey,
+                    onPressed: () => showDialog(
+                      context: context,
+                      builder: (context) => Dialog(
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Add Bookmark',
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 18),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Bookmark Name',
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 15),
+                              ),
+                              TextField(
+                                controller: namecontroller,
+                                decoration: InputDecoration(
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 2, color: Colors.green)),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 2, color: Colors.green)),
+                                ),
+                                cursorHeight: 18,
+                                cursorColor: Colors.green,
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Bookmark URL',
+                                style: TextStyle(
+                                    color: Colors.green, fontSize: 15),
+                              ),
+                              TextField(
+                                decoration: InputDecoration(
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 2, color: Colors.green)),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          width: 2, color: Colors.green)),
+                                ),
+                                cursorHeight: 18,
+                                cursorColor: Colors.green,
+                              ),
+                              ButtonBar(
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Cancel',
+                                        style: TextStyle(color: Colors.grey)),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        name = namecontroller.text;
+                                      });
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      'Save',
+                                      style: TextStyle(color: Colors.green),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.add_circle,
+                      color: Colors.grey[100],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _opensearch() {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SafeArea(
+          child: Scaffold(
+            body: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>Home()), (route) => false);
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_outlined,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        height: 35,
+                        width: 280,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      width: 3, color: Colors.green)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      width: 3, color: Colors.green)),
+                              prefixIcon: Container(
+                                height: 10,
+                                width: 10,
+                                child: Image.asset('assets/images/google.png'),
+                              ),
+                              hintText: 'Search Or Enter Url',
+                              hintStyle: TextStyle(fontSize: 12)),
+                          cursorColor: Colors.green,
+                          cursorHeight: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
