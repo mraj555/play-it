31bff7a6041967bac65b83507d61ba11bff53017
Nehal <nehalsatasiya@gmail.com>
2022-03-04 13:15:36 +0530

    Nehal

--- lib/Nehal/Me/About us.dart
+++ lib/Nehal/Me/About us.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Aboutpage extends StatefulWidget {
@@ -19,6 +20,14 @@ class _AboutpageState extends State<Aboutpage> {
           'About us',
           style: TextStyle(fontSize: 25),
         ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding:  EdgeInsets.only(left: 165,top: 30),
+            child: Image.network('https://sg-res.9appsinstall.com/sg/res/jpg/f8/1b/9d040350ec8e74e93e6f2df32a9b-qde3.jpg',height: 70,width: 70,),
+          ),
+        ],
       ),
     );
   }
