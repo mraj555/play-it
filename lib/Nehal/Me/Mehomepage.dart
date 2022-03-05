bd562e797424f45a71d254abdf3a3ac86fdbfa31
mraj555 <jenilambaliya999@gmail.com>
2022-03-05 17:22:14 +0530

    File Added

--- lib/Nehal/Me/Mehomepage.dart
+++ lib/Nehal/Me/Mehomepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:play_it/Download/_Privacy.dart';
 import 'package:play_it/Download/download.dart';
 import 'package:play_it/Nehal/Me/About%20us.dart';
 import 'package:play_it/Nehal/Me/Settings.dart';
@@ -16,7 +17,6 @@ class Myme extends StatefulWidget {
 }
 
 class _MyappState extends State<Myme> {
-
   static var a = 'assets/Icons/ic_launcher.png';
   static var b = 'assets/Icons/ic_launcher.png';
   static var c = 'assets/Icons/ic_launcher.png';
@@ -24,18 +24,17 @@ class _MyappState extends State<Myme> {
   static var e = 'assets/Icons/ic_launcher.png';
   static var f = 'assets/Icons/ic_launcher.png';
 
-  static var text='Rate Us';
+  static var text = 'Rate Us';
   final _dialog = Dialog(
     backgroundColor: Colors.transparent,
     insetPadding: EdgeInsets.zero,
-
     child: StatefulBuilder(
       builder: (BuildContext context, void Function(void Function()) setState) {
         return Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Padding(
-              padding: const EdgeInsets.only(right: 50,left: 50),
+              padding: const EdgeInsets.only(right: 50, left: 50),
               child: Container(
                 color: Colors.white,
                 child: Column(
@@ -44,119 +43,154 @@ class _MyappState extends State<Myme> {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 60),
-                      child: Text("$text",style: TextStyle(fontSize: 20,color: Colors.black,fontWeight: FontWeight.bold),),
+                      child: Text(
+                        "$text",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 20,top: 25,right: 10),
-                      child: Text("If you like PLAYit,please give us flive stars on the Google Play",style: TextStyle(fontSize: 15,color: Colors.black),),
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 25, right: 10),
+                      child: Text(
+                        "If you like PLAYit,please give us flive stars on the Google Play",
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 45,top: 20),
+                      padding: const EdgeInsets.only(left: 45, top: 20),
                       child: Row(
                         children: [
                           InkWell(
-                            onTap: (){
-                              setState(() {
-                                _star=!_star;
-                                if(_star==true){
-                                  _star=true;
-                                  _star1=false;
-                                  _star2=false;
-                                  _star3=false;
-                                  _star4=false;
-                                }
-                                if(_star==false) {
-                                  _star1 = false;
-                                  _star2 = false;
-                                  _star3 = false;
-                                  _star4 = false;
-                                }
-                              },
+                            onTap: () {
+                              setState(
+                                () {
+                                  _star = !_star;
+                                  if (_star == true) {
+                                    _star = true;
+                                    _star1 = false;
+                                    _star2 = false;
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                  if (_star == false) {
+                                    _star1 = false;
+                                    _star2 = false;
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                },
                               );
                             },
-                            child: Icon(_star? Icons.star : Icons.star_border,size: 35,color:_star ? Colors.amber : Colors.black,),
+                            child: Icon(
+                              _star ? Icons.star : Icons.star_border,
+                              size: 35,
+                              color: _star ? Colors.amber : Colors.black,
+                            ),
                           ),
                           InkWell(
-                            onTap: (){
-                              setState(() {
-                                _star1=!_star1;
-                                if(_star1==true){
-                                  _star=true;
-                                  _star1=true;
-                                  _star2=false;
-                                  _star3=false;
-                                  _star4=false;
-                                }
-                                if(_star1==false) {
-                                  _star2 = false;
-                                  _star3 = false;
-                                  _star4 = false;
-                                }
-                              },
+                            onTap: () {
+                              setState(
+                                () {
+                                  _star1 = !_star1;
+                                  if (_star1 == true) {
+                                    _star = true;
+                                    _star1 = true;
+                                    _star2 = false;
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                  if (_star1 == false) {
+                                    _star2 = false;
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                },
                               );
                             },
-                            child: Icon(_star1? Icons.star : Icons.star_border,size: 35,color:_star1 ? Colors.amber : Colors.black,),
+                            child: Icon(
+                              _star1 ? Icons.star : Icons.star_border,
+                              size: 35,
+                              color: _star1 ? Colors.amber : Colors.black,
+                            ),
                           ),
                           InkWell(
-                            onTap: (){
-                              setState(() {
-                                _star2=!_star2;
-                                if(_star2==true){
-                                  _star=true;
-                                  _star1=true;
-                                  _star2=true;
-                                  _star3=false;
-                                  _star4=false;
-                                }
-                                if(_star2==false) {
-                                  _star3 = false;
-                                  _star4 = false;
-                                }
-                              },
+                            onTap: () {
+                              setState(
+                                () {
+                                  _star2 = !_star2;
+                                  if (_star2 == true) {
+                                    _star = true;
+                                    _star1 = true;
+                                    _star2 = true;
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                  if (_star2 == false) {
+                                    _star3 = false;
+                                    _star4 = false;
+                                  }
+                                },
                               );
                             },
-                            child: Icon(_star2? Icons.star : Icons.star_border,size: 35,color:_star2 ? Colors.amber : Colors.black,),
+                            child: Icon(
+                              _star2 ? Icons.star : Icons.star_border,
+                              size: 35,
+                              color: _star2 ? Colors.amber : Colors.black,
+                            ),
                           ),
                           InkWell(
-                            onTap: (){
-                              setState(() {
-                                _star3=!_star3;
-                                if(_star3==true){
-                                  _star=true;
-                                  _star1=true;
-                                  _star2=true;
-                                  _star3=true;
-                                  _star4=false;
-                                }
-                                if(_star3==false) {
-                                  _star4 = false;
-                                }
-                              },
+                            onTap: () {
+                              setState(
+                                () {
+                                  _star3 = !_star3;
+                                  if (_star3 == true) {
+                                    _star = true;
+                                    _star1 = true;
+                                    _star2 = true;
+                                    _star3 = true;
+                                    _star4 = false;
+                                  }
+                                  if (_star3 == false) {
+                                    _star4 = false;
+                                  }
+                                },
                               );
                             },
-                            child: Icon(_star3? Icons.star : Icons.star_border,size: 35,color:_star3 ? Colors.amber : Colors.black,),
+                            child: Icon(
+                              _star3 ? Icons.star : Icons.star_border,
+                              size: 35,
+                              color: _star3 ? Colors.amber : Colors.black,
+                            ),
                           ),
                           InkWell(
-                            onTap: (){
-                              setState(() {
-                                _star4=!_star4;
-                                if(_star4==true){
-                                  _star=true;
-                                  _star1=true;
-                                  _star2=true;
-                                  _star3=true;
-                                  _star4=true;
-                                }
-                              },
+                            onTap: () {
+                              setState(
+                                () {
+                                  _star4 = !_star4;
+                                  if (_star4 == true) {
+                                    _star = true;
+                                    _star1 = true;
+                                    _star2 = true;
+                                    _star3 = true;
+                                    _star4 = true;
+                                  }
+                                },
                               );
                             },
-                            child: Icon(_star4? Icons.star : Icons.star_border,size: 35,color:_star4 ? Colors.amber : Colors.black,),
+                            child: Icon(
+                              _star4 ? Icons.star : Icons.star_border,
+                              size: 35,
+                              color: _star4 ? Colors.amber : Colors.black,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    ElevatedButton(onPressed: (){}, child: Text("Submit")),
-                    ElevatedButton(onPressed: (){}, child: Text("Exit")),
+                    ElevatedButton(onPressed: () {}, child: Text("Submit")),
+                    ElevatedButton(onPressed: () {}, child: Text("Exit")),
                   ],
                 ),
               ),
@@ -164,7 +198,11 @@ class _MyappState extends State<Myme> {
             Baseline(
               baseline: -240,
               baselineType: TextBaseline.ideographic,
-              child: Image.asset('$a',height: 100,width: 100,),
+              child: Image.asset(
+                '$a',
+                height: 100,
+                width: 100,
+              ),
             ),
           ],
         );
@@ -173,11 +211,11 @@ class _MyappState extends State<Myme> {
   );
 
   var _icons = [
-    Icons.download_outlined,
-    Icons.headset_mic_outlined,
-    Icons.folder_outlined,
-    Icons.access_time_outlined,
-    Icons.folder_outlined,
+    Icon(Icons.download_outlined),
+    Icon(Icons.headset_mic_outlined),
+    ImageIcon(AssetImage('assets/Icons/privacy.png')),
+    Icon(Icons.access_time_outlined),
+    Icon(Icons.folder_outlined),
   ];
 
   var _names = [
@@ -191,16 +229,16 @@ class _MyappState extends State<Myme> {
   var _pagelist = [
     Down(),
     Down(),
-    Down(),
+    Privacy(),
     Down(),
     Down(),
   ];
 
-  static var _star=false;
-  static var _star1=false;
-  static var _star2=false;
-  static var _star3=false;
-  static var _star4=false;
+  static var _star = false;
+  static var _star1 = false;
+  static var _star2 = false;
+  static var _star3 = false;
+  static var _star4 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -358,10 +396,9 @@ class _MyappState extends State<Myme> {
                                   mainAxisSize: MainAxisSize.min,
                                   children: [
                                     GestureDetector(
-                                      child: Icon(
-                                        _icons[index],
-                                        color: Colors.white.withOpacity(0.9),
-                                        size: 30,
+                                      child: IconTheme(
+                                        data: IconThemeData(color: Colors.white,size: size.height *0.05),
+                                        child: _icons[index],
                                       ),
                                       onTap: () {
                                         Navigator.push(
