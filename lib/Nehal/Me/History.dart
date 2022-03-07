037bc742d7db203f4b67cdaac28d264d23b5bae9
Nehal <nehalsatasiya@gmail.com>
2022-03-07 12:41:14 +0530

    Nehal

---
+++ lib/Nehal/Me/History.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class History extends StatefulWidget {
+  const History({Key? key}) : super(key: key);
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff0E0E0E),
+      appBar: AppBar(
+        backgroundColor: Color(0xff0E0E0E),
+        centerTitle: true,
+        title: Text("History"),
+      ),
+    );
+  }
+}
