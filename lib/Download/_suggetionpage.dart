5d0eb5c36709396061b8a3bac38cbe8be723bb5b
mraj555 <jenilambaliya999@gmail.com>
2022-03-11 11:48:14 +0530

    File Added

--- lib/Download/_suggetionpage.dart
+++ lib/Download/_suggetionpage.dart
@@ -16,12 +16,7 @@ class _SuggestState extends State<Suggest> {
     'assets/images/page3.jpg',
     'assets/images/page4.jpg',
   ];
-  List text = [
-    '1.Enter the Website',
-    '2.Play A video',
-    '3.Tap Download Button',
-    '4.Youtube Player',
-  ];
+  var text='1.Enter the Website';
   List skip = [
     'Next',
     'Got it',
@@ -34,60 +29,64 @@ class _SuggestState extends State<Suggest> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff413f42),
-      body: PageView(
-        controller: controller,
-        children: List.generate(
-          _image.length,
-          (index) => Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: size.height * 0.03),
-                height: size.height * 0.6,
-                child: Image.asset(
-                  _image[index],
-                  fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Container(
+            height: 380,
+            child: PageView(
+                controller: controller,
+                children: List.generate(
+                  _image.length,
+                  (index) => Container(
+                    margin: EdgeInsets.only(top: size.height * 0.03),
+                    height: size.height * 0.6,
+                    child: Image.asset(
+                      _image[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                height: size.height * 0.07,
-                child: SmoothPageIndicator(
-                  controller: controller,
-                  count: 4,
-                  effect: JumpingDotEffect(
-                      dotWidth: 8,
-                      dotHeight: 8,
-                      dotColor: Colors.grey.shade400,
-                      activeDotColor: Colors.white),
-                ),
-              ),
-              Text(
-                'How to download video and audio ?',
-                style:
-                    TextStyle(fontSize: size.width * 0.04, color: Colors.grey),
-              ),
-              SizedBox(height: size.height * 0.02),
-              Text(
-                text[index],
-                style:
-                    TextStyle(fontSize: size.width * 0.06, color: Colors.white),
-              ),
-              Spacer(),
-              ElevatedButton(
-                onPressed: () {
-                  controller.nextPage(duration: Duration(seconds: 1), curve: Curves.fastLinearToSlowEaseIn);
-                },
-                child: Text(
-                  skip[index],
-                  style: TextStyle(
-                      color: Colors.white, fontSize: size.height * 0.025),
-                ),
-                style: ElevatedButton.styleFrom(
-                    primary: Color(0xff2bc877),
-                    fixedSize: Size(size.width * 0.4, size.height * 0.04)),
-              ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            height: size.height * 0.07,
+            child: SmoothPageIndicator(
+              controller: controller,
+              count: 4,
+              effect: JumpingDotEffect(
+                  dotWidth: 8,
+                  dotHeight: 8,
+                  dotColor: Colors.grey.shade400,
+                  activeDotColor: Colors.white),
+            ),
+          ),
+          Text(
+            'How to download video and audio ?',
+            style: TextStyle(
+                fontSize: size.width * 0.04, color: Colors.grey),
+          ),
+          SizedBox(height: size.height * 0.02),
+          Text(
+            '${text}',
+            style: TextStyle(
+                fontSize: size.width * 0.06, color: Colors.white),
+          ),
+          Spacer(),
+          ElevatedButton(
+            onPressed: () {
+              controller.nextPage(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.fastLinearToSlowEaseIn);
+            },
+            child: Text('Next',
+              style: TextStyle(
+                  color: Colors.white, fontSize: size.height * 0.025),
+            ),
+            style: ElevatedButton.styleFrom(
+                primary: Color(0xff2bc877),
+                fixedSize: Size(size.width * 0.4, size.height * 0.04)),
+          ),
+        ],
       ),
     );
   }
